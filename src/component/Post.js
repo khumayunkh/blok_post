9033362cda0852b3a0e415ff2534a002df801c07
khumayunkh <hasanovhumayun15@gmail.com>
2022-07-02 23:34:46 +0500

    js

---
+++ src/component/Post.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Posts(){
+    <>
+    <h1>hello</h1>
+    </>
+}
+
+export default Posts
---
+++ src/store/PostSlice.js
@@ -0,0 +1,52 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import axios from "axios"
+
+
+export const fetchTodos = createAsyncThunk(
+    'todos/fetchTodos',
+    async function(){
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+        const data = response.data
+    }
+)
+
+export const addNewTodos = createAsyncThunk(
+    'todos/addNewTodos',
+    async function(text, {rejectWithValue, dispatch}){
+        const todo = {
+            title: text,
+            userId: 1,
+            completed: false,
+        };
+        const response = await axios.post('https://jsonplaceholder.typicode.com/todos',{
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body : todo
+        })
+        const data = await response.data
+        dispatch(addTodos(data))
+    }
+)
+
+const initialState = {
+    todos : [],
+    status : null
+}
+
+
+const todoSlice = createSlice({
+    name : 'todos',
+    initialState : initialState,
+    reducers:{
+        addTodos(state, action){
+            state.todos.push(action.payload)
+        },
+        removeTodos(state, action){
+            state.todos = state.todos.filter(item => item.id !== action.payload.id)
+        }
+    }
+})
+
+export const {addTodos, removeTodos} = todoSlice.actions;
+export default todoSlice.reducer
